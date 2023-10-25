0a6e435b0e0ebc3e8f0889e95563ae524d2a4ee3
Var <146992931+IfVar@users.noreply.github.com>
2023-10-25 09:03:06 -0500

    Update Scripts-G-Library.js

--- Scripts-G-Library.js
+++ Scripts-G-Library.js
@@ -53,3 +53,58 @@ const AdvancedColorNames = [
 /* -------------------------------- Shortcuts ------------------------------- */
 
 const db = document.body
+
+/* ADD COOL STUFF LATER HERE (OnX Functions) */
+
+function OnClick(object, callback) {
+    object.addEventListener("click", callback);
+}
+
+function OnHover(object, callback) {
+    object.addEventListener("mouseover", callback);
+}
+
+function OnLeave(object, callback) {
+    object.addEventListener("mouseout", callback);
+}
+
+function OnDClick(object, callback) {
+    object.addEventListener("dblclick", callback);
+}
+
+function OnRClick(object, callback) {
+    object.addEventListener("contextmenu", callback);
+}
+
+function NoRMenu(object){
+    object.addEventListener("contextmenu", (event) => {event.preventDefault()});
+}
+
+/* ADD COOL STUFF LATER HERE (Class Functions) */
+
+function AddClass(object, Class){
+    object.classList.add(Class);
+}
+
+function RemoveClass(object, Class){
+    object.classList.remove(Class);
+}
+
+function ChangeClass(object, RClass, AClass){
+    object.classList.remove(RClass);
+    object.classList.add(AClass);
+}
+
+function ToggleClass(object, Class){
+    object.classList.toggle(Class);
+}
+
+/* ADD COOL STUFF LATER HERE (Link Functions) */
+
+function Goto(Link){
+    window.location.href(Link)
+}
+
+function NewTab(Link){
+    window.open(Link)
+}
